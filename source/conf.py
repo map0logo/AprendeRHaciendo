d388866b27a78e877cf2d4405cb51575f6ef2f8d
Francisco Palm <francisco.palm@gmail.com>
2013-11-28 14:03:16 -0430

    Set for readthedocs.org

--- source/conf.py
+++ source/conf.py
@@ -109,7 +109,11 @@ else:
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+if on_rtd:
+    html_theme = []
+else:
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
